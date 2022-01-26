70ec59d377fcb2dfec4e2074cec97d458001e295
hectorchu~ <hector.chu828@gmail.com>
2022-01-26 15:24:55 -0800

    1 failure, test_all_in_one

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Put your app in here.
 from flask import Flask, request
 import operations
 
@@ -7,30 +6,30 @@ app = Flask(__name__)
 @app.get("/add")
 def math_add():
     """this function accepts two params and return the sum"""
-    term_a = request.args.get('<int:a>')
-    term_b = request.args.get('<int:b>')
-    return f"""{operations.add(term_a,term_b)}"""
+    term_a = request.args.get('a')
+    term_b = request.args.get('b')
+    return f"""{operations.add(int(term_a),int(term_b))}"""
 
 @app.get('/sub')
 def math_sub():
     """this function accepts two params and return the subtraction of two params"""
 
-    term_a = request.args.get('<int:a>')
-    term_b = request.args.get('<int:b>')
-    return f"""{operations.add(term_a,term_b)}"""
+    term_a = request.args.get('a')
+    term_b = request.args.get('b')
+    return f"""{operations.sub(int(term_a), int(term_b))}"""
 
 @app.get('/mult')
 def math_mult():
     """this function accepts two params and return the product of two params"""
 
-    term_a = request.args.get('<int:a>')
-    term_b = request.args.get('<int:b>')
-    return f"""{operations.add(term_a,term_b)}"""
+    term_a = request.args.get('a')
+    term_b = request.args.get('b')
+    return f"""{operations.mult(int(term_a), int(term_b))}"""
 
 @app.get('/div')
 def math_div():
     """this function accepts two params and return the division of two params"""
 
-    term_a = request.args.get('<int:a>')
-    term_b = request.args.get('<int:b>')
-    return f"""{operations.add(term_a,term_b)}"""
+    term_a = request.args.get('a')
+    term_b = request.args.get('b')
+    return f"""{operations.div(int(term_a), int(term_b))}"""
