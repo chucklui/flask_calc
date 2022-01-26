ac4dec56d5d4a2d935b7c732f6133b964c49cdb5
Chuck Lui <luialex0@gmail.com>
2022-01-26 15:42:18 -0800

    finished cal for flask

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
 from flask import Flask, request
-import operations
+from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
 @app.get("/add")
+# @app.get("/math/add")
 def math_add():
     """this function accepts two params and return the sum"""
+
     term_a = request.args.get('a')
     term_b = request.args.get('b')
-    return f"""{operations.add(int(term_a),int(term_b))}"""
+    return str(add(int(term_a),int(term_b)))
 
 @app.get('/sub')
+# @app.get('/math/sub')
 def math_sub():
     """this function accepts two params and return the subtraction of two params"""
 
     term_a = request.args.get('a')
     term_b = request.args.get('b')
-    return f"""{operations.sub(int(term_a), int(term_b))}"""
+    return str(sub(int(term_a), int(term_b)))
 
 @app.get('/mult')
+# @app.get('/math/mult')
 def math_mult():
     """this function accepts two params and return the product of two params"""
 
     term_a = request.args.get('a')
     term_b = request.args.get('b')
-    return f"""{operations.mult(int(term_a), int(term_b))}"""
+    return str(mult(int(term_a), int(term_b)))
 
 @app.get('/div')
+# @app.get('/math/div')
 def math_div():
     """this function accepts two params and return the division of two params"""
 
     term_a = request.args.get('a')
     term_b = request.args.get('b')
-    return f"""{operations.div(int(term_a), int(term_b))}"""
+    return str(div(int(term_a), int(term_b)))
